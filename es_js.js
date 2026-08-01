da9359368e7e507148d5891dc2d6b3dfcc10f7ee
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-08-01 21:20:09 +0600

    default parameter

--- es_js.js
+++ es_js.js
@@ -1,1 +1,11 @@
-console.log(alert("hello"))
+function defaultName(name = "shoron"){
+    return "hellow, " + name;
+}
+console.log(defaultName("ekhlas"))
+console.log(defaultName())
+console.log(defaultName("siham"))
+
+function calculator (number, tax=50){
+    return number + tax
+}
+console.log(calculator(1000,))
