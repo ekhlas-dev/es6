b43ed96d13156191f0d5414ea09a3bb07a8d0de8
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-08-07 15:34:12 +0600

    new done

--- reduce.js
+++ reduce.js
@@ -26,7 +26,31 @@
 // },0)
 // console.log(totalWordCount)
 
-const numbers = [2, 7, 10, 15, 18, 21];
+// const numbers = [2, 7, 10, 15, 18, 21];
 
-const countEvenNumber = numbers.filter((num) => num % 2 !== 0);
-console.log(countEvenNumber)
+// const countEvenNumber = numbers.filter((num) => num % 2 !== 0);
+// console.log(countEvenNumber)
+
+// function calculateDiscountTotal(discount, ...prices) {
+//   const totalPriceSum = prices.reduce((accumulator, cuunetValue) => {
+//     return accumulator + cuunetValue;
+//   }, 0);
+//   const discountAmount = (discount * totalPriceSum) / 100;
+//   let totalAfterDiscount = totalPriceSum - discountAmount
+//   return totalAfterDiscount
+// }
+// console.log(calculateDiscountTotal(10, 100, 200, 50, 300))
+
+function productDiscount (discount, ...prices){
+    const total = prices.reduce((accumulator,currentVaule)=>accumulator+currentVaule,0)
+    const discountAmount = (discount * total) /100
+    const totalAfterDiscount = total - discountAmount
+    return totalAfterDiscount
+}
+console.log(productDiscount(10,100,200,50,300))
+
+let arr1 = [1,2,3,4,5,7,8,9]
+let arr2 = [2,3,5,6,7,88,9,1]
+let margeArray = [...arr1,...arr2]
+console.log(margeArray)
+let newArray = [...new Set(margeArray)]
