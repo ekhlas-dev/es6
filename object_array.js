eed32c925876fae7ced2e44eba38756436509248
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-08-08 22:08:10 +0600

    problem solve done

--- object_array.js
+++ object_array.js
@@ -18,22 +18,44 @@
 
 // console.log(addGrade(students));
 
-const addGrade =(students)=>{
-    const getMark =(mark)=>{
-        return mark > 90?"A+":mark>80 ? "A" : mark > 70 ? "B" : "fail"
+// const addGrade =(students)=>{
+//     const getMark =(mark)=>{
+//         return mark > 90?"A+":mark>80 ? "A" : mark > 70 ? "B" : "fail"
+//     }
+//     const modifiStudent = students.map((student)=>{
+//         const {name,mark} = student
+//         const newStudentObj = {name,mark, grade: getMark(mark)}
+//         return newStudentObj
+//     })
+//     return modifiStudent
+// }
+
+// const students = [
+//   { name: "ekhlas", mark: 85 },
+//   { name: "karim", mark: 93 },
+//   { name: "rahim,", mark: 50 },
+// ];
+
+// console.log(addGrade(students))
+
+
+const calculator = (products)=>{
+    let totalItems = 0;
+    let totalPrice = 0;
+    for(let product of products){
+        const {name,price,qty} = product
+        totalItems += qty
+        totalPrice +=(price*qty)
     }
-    const modifiStudent = students.map((student)=>{
-        const {name,mark} = student
-        const newStudentObj = {name,mark, grade: getMark(mark)}
-        return newStudentObj
-    })
-    return modifiStudent
+    console.log(totalItems,totalPrice)
+    return `total ${totalItems || 0} items ${totalPrice || 0} Taka`
 }
 
-const students = [
-  { name: "ekhlas", mark: 85 },
-  { name: "karim", mark: 93 },
-  { name: "rahim,", mark: 50 },
-];
 
-console.log(addGrade(students))
+
+const products =[
+    //  { name: "pen",price:20, qty: 2 },
+    //  { name: "noteBook",price:200, qty: 3 },
+     { name: "hadephone",price:0, qty: 0 },
+]
+console.log(calculator(products))
