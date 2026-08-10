e3f210e1655ca180839b84e85acd033349b77bd1
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-08-10 14:15:29 +0600

    assanding problem done

--- array_method.js
+++ array_method.js
@@ -28,10 +28,10 @@ let student = [
 // problem 3
 
 const findUserById = (users, id) => {
-  let findUser = users.find((user)=>{
+  let findUser = users.find((user) => {
     return user.id === id;
-  })
-  if(!findUser) return "user not found"
+  });
+  if (!findUser) return "user not found";
 };
 
 const users = [
@@ -43,4 +43,51 @@ const users = [
 // console.log(findUserById(users,22))
 
 // problem 4
+
+const getCartTotal = (cart) => {
+  let total = cart.reduce((accumulator, element) => {
+    return accumulator + element.price * element.qty;
+  }, 0);
+  return total;
+};
+
+// let cart = [
+//   { name: "pen", price: 30, qty: 3 },
+//   { name: "notebook", price: 70, qty: 2 },
+//   { name: "book", price: 300, qty: 5 },
+//   { name: "bag", price: 1000, qty: 1 },
+// ];
+// console.log(getCartTotal(cart))
+
+// problem 5
+
+let getExpensiveItemsTotal = (carts, minPrice) => {
+  let total = carts
+    .filter((product) => product.price >= minPrice)
+    .map((product) => product.price * product.qty)
+    .reduce((accumulator,currentValue)=>accumulator+currentValue);
+    return total
+};
+
+let carts = [
+  { name: "pen", price: 30, qty: 3 },
+  { name: "notebook", price: 700, qty: 2 },
+  { name: "penBook", price: 100, qty: 3 },
+  { name: "book", price: 800, qty: 5 },
+  { name: "bag", price: 1000, qty: 1 },
+  { name: "hedphone", price: 500, qty: 3 },
+];
+// console.log(getExpensiveItemsTotal(carts, 500));
+
+// problem 6
+
+const sortDescending = (numbers)=>{
+  const number = [...numbers]
+  const desenDingNumber = number.sort((a,b)=>b-a)
+  return desenDingNumber
+}
+
+const numbers = [1,10,2,25,3]
+// console.log(sortDescending(numbers))
+// console.log(numbers)
 
